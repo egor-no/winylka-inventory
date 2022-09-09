a4d0050167f8c281e690455545dffa55e4ccf886
egor-no <e.nosko93@ya.ru>
2022-09-09 20:43:53 +0900

    JAX RS project setup

---
+++ winylka-catalog-jax/src/main/java/com/winylka/WinylkaRestApplication.java
@@ -0,0 +1,9 @@
+package com.winylka;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/winylka/api")
+public class WinylkaRestApplication extends Application {
+
+}
